39f90267f24e7b32eab44baf4809c2f63e6cba6d
Anderson S Souza <pfanderson3@gmail.com>
2022-09-09 11:55:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+  })
+  const page = await browser.newPage()
+
+  await page.setViewportSize({ width: 1280, height: 800 })
+  await page.goto('https://www.bet365.com/#/AVR/B144/')
+  await page.waitForTimeout(7000)
+  await page.screenshot({ path: 'my_screenshot.png' })
+  await browser.close()
+})()
